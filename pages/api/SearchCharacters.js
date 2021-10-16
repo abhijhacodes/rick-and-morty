335d0325d5c5cb4362690c293ad766348fd2384f
abhijhacodes <abhi.jha.cs@gmail.com>
2021-10-16 05:40:42 +0530

    initial commit

---
+++ pages/api/SearchCharacters.js
@@ -0,0 +1,52 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql/",
+  cache: new InMemoryCache(),
+});
+
+export default async (req, res) => {
+  const search = req.body;
+  try {
+    const { data } = await client.query({
+      query: gql`
+        query {
+          characters(filter: { name: "${search}" }) {
+            info {
+              count
+            }
+            results {
+              name
+              id
+              location {
+                name
+                id
+              }
+              image
+              origin {
+                name
+                id
+              }
+              episode {
+                id
+                episode
+                air_date
+              }
+            }
+          }
+        }
+      `,
+    });
+    res.status(200).json({ characters: data.characters.results, error: null });
+  } catch (error) {
+    if (error.message === "404: Not Found") {
+      res
+        .status(404)
+        .json({ characters: null, error: "Please enter a valid name" });
+    } else {
+      res
+        .status(500)
+        .json({ characters: null, error: "Internal Error, Please try again" });
+    }
+  }
+};
